c5c58897180f5388c592f0a7be0f297ace28bb2b
Santosh <sahosantosh117@gmail.com>
2025-05-13 22:31:45 +0530

    intial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-4xl '>Welcome to Netflix-Gpt</div>
+    </>
+  )
+}
+
+export default App
